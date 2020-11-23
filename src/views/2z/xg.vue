662ba1556707885f1a5929ce407df06fed397749
j980329 <291433933@qq.com>
2020-11-23 16:54:19 +0800

    三号项目第一次上传

--- src/views/2z/xg.vue
+++ src/views/2z/xg.vue
@@ -1,34 +1,170 @@
 <template>
-    <div>
-        <h1>流程</h1>
-    </div>
+  <div>
+    <header class="header">
+      <div class="fd">
+        <img src="@/assets/imgs/logo.png" alt class="log" />
+      </div>
+      <div class="lxlc fd">装修流程</div>
+      <div class="fd gd">
+        <div class="fd">
+          <div class="iconfont icon-user wd hs to"></div>
+          <p class="jz hd me">我的</p>
+        </div>
+
+        <div class="fd mz">
+          <div class="iconfont icon-nav dh hs to"></div>
+          <p class="jz hd me">导航</p>
+        </div>
+      </div>
+    </header>
+    <!-- //红彤彤的图 -->
+    <div class="bghear"></div>
+    <!-- 接下来的图 -->
+    <div class="bg1"></div>
+    <!-- <div class="bg2"> -->
+    <!-- <img :src="imgSrc2" width="100%" height="100%" alt="" /> -->
+    <!-- </div> -->
+    <!-- <div class="bg3"> -->
+    <!-- <img :src="imgSrc3" width="100%" height="100%" alt="" /> -->
+    <!-- </div> -->
+    <!-- <div class="bg4"> -->
+    <!-- <img :src="imgSrc4" width="100%" height="100%" alt="" /> -->
+    <!-- </div> -->
+    <!-- <div class="bg5"> -->
+    <!-- <img :src="imgSrc5" width="100%" height="100%" alt="" /> -->
+    <!-- </div> -->
+    <div class="bg2"></div>
+    <div class="bg3"></div>
+    <div class="hsd"></div>
+    <p class="zt">
+      房先森互联网家装有别于传统装修公司与其他装修中
+      介平台， 以自营模式整合设计师与项目经理， 杜绝项目转
+      包外包， 利用互联网将工地进行在线全景直播， 通过去中
+      间环节， 提供装修套餐+ 个性化定制服务。
+    </p>
+    <div class="hsd"></div>
+    <div class="bg4"></div>
+  </div>
 </template>
 
 <script>
+import rem from "@/assets/rem";
+import  "../../assets/iconfont/iconfont.css"
 export default {
-    data() {
-        return {
-
-        };
-    },
-    created() {
-
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    },
-    computed:{
-
-    },
-    components:{
-
-    }
+  data() {
+    return {
+      // imgSrc1:require('@/imgs/process/proess1.jpg'),
+      // imgSrc2:require('@/imgs/process/proess2.jpg'),
+      // imgSrc3:require('@/imgs/process/proess3.jpg'),
+      // imgSrc4:require('@/imgs/process/proess4.jpg'),
+      // imgSrc5:require('@/imgs/process/proess5.jpg'),
+      // imgSrc:require('../../图片/切片/process/process1.jpg'),
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  components: {}
 };
 </script>
 
-<style scoped>
-
+<style>
+html {
+  height: 100%;
+}
+*p {
+  padding: 0px;
+  margin: 0px;
+  list-style: none;
+}
+body {
+  height: 100%;
+  margin: 0px;
+  /* display: flex; */
+  /* flex-direction: column; */
+  font-size: 0.16rem;
+}
+.lxlc {
+  font-size: 0.36rem;
+  color: #000;
+  width: 2.33rem;
+  /* margin-left: 0.74rem; */
+  margin-right: 0.44rem;
+}
+.wd {
+  font-size: 0.43rem;
+}
+.dh {
+  font-size: 0.43rem;
+}
+.hs {
+  color: #999999;
+}
+.fd {
+  float: left;
+}
+.jz {
+  text-align: center;
+}
+.me {
+  font-size: 0.2rem;
+}
+.log {
+  width: 2.33rem;
+}
+p{
+    margin: 0;
+}
+.gd {
+  width: 1.9rem;
+  padding-left: 0.5rem;
+}
+.mz {
+  margin-left: 0.38rem;
+}
+.bghear {
+  background: url("../../assets/imgs/process1.jpg");
+  width: 100%;
+  height: 2.46rem;
+  background-size: 100% 100%;
+}
+.bg1 {
+  background: url("../../assets/imgs/process2.jpg");
+  width: 100%;
+  height: 8.55rem;
+  background-size: 100% 100%;
+}
+.bg2 {
+  background: url("../../assets/imgs/process3.png");
+  width: 100%;
+  height: 8.89rem;
+  background-size: 100% 100%;
+}
+.bg3 {
+  background: url("../../assets/imgs/process4.jpg");
+  width: 100%;
+  height: 9.49rem;
+  background-size: 100% 100%;
+}
+.bg4 {
+  background: url("../../assets/imgs/process5.jpg");
+  width: 100%;
+  height: 2.73rem;
+  background-size: 100% 100%;
+}
+.header {
+  height: 1rem;
+}
+.hsd {
+  height: 0.19rem;
+  background: #eaeaea;
+}
+.zt {
+  font-size: 0.26px;
+  text-indent: 0.52em;
+}
+.to{
+    margin-top: 5px;
+}
 </style>
