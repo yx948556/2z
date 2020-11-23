cc05a3ca7174d4022a2731efb496d49a7544f553
myYX <948556346@qq.com>
2020-11-23 20:07:35 +0800

    项目

--- src/views/2z/sj.vue
+++ src/views/2z/sj.vue
@@ -1,34 +1,563 @@
 <template>
-    <div>
-
+  <div class="yx_box">
+    <!-- 头部布局 -->
+    <div class="yx_hea">
+      <div class="yx_hea-img">
+        <img src="@/assets/imgs/logo.png" alt />
+      </div>
+      <div class="yx_hea-txt">设计师</div>
+      <div class="yx_hea-ico">
+        <div>
+          <i class="iconfont icon-user"></i>
+          <p>我的</p>
+        </div>
+        <div>
+          <i class="iconfont icon-nav"></i>
+          <p>导航</p>
+        </div>
+      </div>
     </div>
+    <!-- 导航 -->
+    <div class="yx_nav">
+      <ul>
+        <li>
+          筛选
+          <i class="iconfont icon-down"></i>
+        </li>
+        <li>
+          风格
+          <i class="iconfont icon-down"></i>
+        </li>
+        <li>
+          排序
+          <i class="iconfont icon-down"></i>
+        </li>
+        <li>
+          <i style="font-size:30px;color:#39c894;" class="iconfont icon-search"></i>
+        </li>
+      </ul>
+    </div>
+    <!-- 图文列表 -->
+    <div class="yx_img">
+      <ul>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="yx_img-img">
+            <img src="@/assets/imgs/lb4.jpg" alt />
+          </div>
+          <div class="yx_img-text">
+            <div class="yx_img-text-p1">
+              <div>
+                <span>陈冲</span>
+                <span class="yx_you">优秀</span>
+              </div>
+              <div>
+                <span class="yx_ren">738</span>
+                <span>人气</span>
+              </div>
+            </div>
+            <p>
+              <span>作品:</span>
+              <span class="yx_num">8</span>
+              <span>经验:</span>
+              <span class="yx_num">5年</span>
+              <span>评价:</span>
+              <span>10</span>
+            </p>
+            <p>
+              <span>擅长风格:</span>
+              <span class="yx_num">现代/美式/简约/新古典</span>
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!-- 图文和底部的中间区域 -->
+    <div class="yx_up">
+      <ul>
+        <li class="yx_li">上一页</li>
+        <li>1/30</li>
+        <li class="yx_up-up">下一页</li>
+      </ul>
+    </div>
+    <!-- 底部的布局 -->
+    <footer>
+      <div class="yx_text4">
+        <p>
+          房先森互联网家装有别于传统装修公司与其他装修中介平台，
+          以自营模式整合设计师与项目经理，杜绝项目转包外包，利用互联网
+          将工地进行在线全景直播，通过去中间环节，提供装修套餐+个性化定制服务
+        </p>
+      </div>
+      <div class="yx_foot">
+        <img src="@/assets/imgs/process5.jpg" alt />
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
+import "@/assets/iconfont/iconfont.css";
 export default {
-    data() {
-        return {
-
-        };
-    },
-    created() {
-
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    },
-    computed:{
-
-    },
-    components:{
-
-    }
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  components: {}
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.yx_box {
+  width: 100%;
+  background: #eaeaea;
+  // 头部布局
+  .yx_hea {
+    width: 100%;
+    height: 102px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #999999;
+    background: #fff;
+    .yx_hea-img {
+      height: 100%;
+      width: 40%;
+      padding: 20px;
+      box-sizing: border-box;
+      img {
+        width: 100%;
+      }
+    }
+    .yx_hea-txt {
+      width: 30%;
+      height: 100%;
+      text-align: center;
+      line-height: 80px;
+      font-size: 24px;
+    }
+    .yx_hea-ico {
+      width: 30%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      i {
+        margin-left: 20px;
+        font-size: 24px;
+        color: #999999;
+      }
+      p {
+        margin-left: 15px;
+        color: #999999;
+      }
+    }
+  }
+  // 导航
+  .yx_nav {
+    width: 100%;
+    height: 102px;
+    ul {
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background: #fff;
+      li {
+        width: 25%;
+        height: 54px;
+        margin-top: 20px;
+        border-right: 1px solid #eaeaea;
+        background: #fff;
+        font-size: 24px;
+        text-align: center;
+        line-height: 54px;
+      }
+    }
+  }
+  // 图文列表
+  .yx_img {
+    width: 100%;
+    margin-top: 20px;
+    ul {
+      width: 100%;
+      padding: 0;
+      li {
+        width: 100%;
+        display: flex;
+        background: #ffff;
+        border-bottom: 1px solid #eaeaea;
+        padding-bottom: 20px;
+        .yx_img-img {
+          width: 30%;
+          box-sizing: border-box;
+          img {
+            width: 80px;
+            margin-top: 36px;
+            margin-left: 20px;
+            margin-right: 20px;
+          }
+        }
 
+        .yx_img-text {
+          width: 68%;
+          .yx_img-text-p1 {
+            width: 100%;
+            height: 40px;
+            margin-top: 20px;
+            display: flex;
+            justify-content: space-between;
+            span {
+              font-size: 18px;
+            }
+            .yx_you {
+              background: #2bb281;
+              padding: 5px;
+              color: #fff;
+            }
+            .yx_ren {
+              color: #eed215;
+            }
+          }
+        }
+        p {
+          margin: 0;
+          margin-top: 10px;
+          .yx_num {
+            color: #999999;
+          }
+          span {
+            margin-right: 13px;
+          }
+        }
+      }
+    }
+  }
+
+  //图文和底部的中间区域
+  .yx_up {
+    width: 100%;
+    height: 100px;
+    box-sizing: border-box;
+    background: #fff;
+    ul {
+      box-sizing: border-box;
+      width: 80%;
+      display: flex;
+      justify-content: space-between;
+      padding-top: 40px;
+      margin-left: 50px;
+      li {
+        width: 33%;
+        text-align: center;
+        border: 1px solid #eaeaea;
+        padding: 5px;
+      }
+      .yx_li {
+        background: #eaeaea;
+      }
+      .yx_up-up {
+        background: #2bb281;
+        color: #fff;
+      }
+    }
+  }
+
+  footer {
+    width: 100%;
+    .yx_text4 {
+      width: 100%;
+      background: #fff;
+      padding: 5px 0;
+      box-sizing: border-box;
+      background: #eaeaea;
+      p {
+        background: #fff;
+      }
+    }
+    .yx_foot {
+      width: 100%;
+      p {
+        text-align: center;
+      }
+      ul {
+        padding: 0;
+        width: 100%;
+        display: flex;
+        li {
+          width: 25%;
+          text-align: center;
+        }
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
 </style>
