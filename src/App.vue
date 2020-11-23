2758fe8c5038885e7481925bdae69e0ddb80f60c
FengCang <1416366899@qq.com>
2020-11-23 19:33:01 +0800

    fc第一次推送

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
+
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-=======
-<<<<<<< HEAD
+
+
     <router-view></router-view>
-=======
+
     <!-- <div id="nav"> -->
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
     <!-- </div> -->
->>>>>>> e52f5c43a23406bc5b0986f6d1da8fa765980a59
+
     <router-view/>
->>>>>>> d699735115ea42dc6466393e7c21732d659f747e
+
   </div>
 </template>
 
