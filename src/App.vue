ad2ed92d72ec3c5fa7292079bfd23805b57b8e71
Lb_lb <570272913@qq.com>
2020-11-24 19:54:33 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-   
-=======
->>>>>>> 45233b3bcdbea31330c29d0149c9a3e235b8dceb
     <router-view/>
   </div>
 </template>
 
 <style>
 
-<<<<<<< HEAD
-=======
-* {
-  margin: 0;
-  padding: 0;
-}
-#app,
-body,
-html {
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-}
-
->>>>>>> 45233b3bcdbea31330c29d0149c9a3e235b8dceb
 </style>
